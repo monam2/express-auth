426e931cc41111654b7bf0dd96d0a677d23bb64c
monam2 <kangcw0107@gmail.com>
2025-03-26 22:06:32 +0900

    jwt token 생성 및 검증

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
-import { jwtSecret } from "../config/env";
+import { jwtSecret } from "../config/env.js";
 
 export const verifyToken = (req, res, next) => {
   let token;
   let authHeader = req.headers.Authorization || req.headers.authorization;
 
-  if (authHeader && authHeader.startWith("Bearer")) {
+  if (authHeader && authHeader.startsWith("Bearer")) {
     token = authHeader.split(" ")[1];
 
     if (!token) {
