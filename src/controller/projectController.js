06f4a5f57fa25486f3326ca3c820893d67fcede2
monam2 <kangcw0107@gmail.com>
2025-03-28 19:02:41 +0900

    update 기능

--- src/controller/projectController.js
+++ src/controller/projectController.js
@@ -1,6 +1,6 @@
 import { projects } from "../data/projectData.js";
 import { authorize } from "../middleware/authorize.js";
-import { canViewProject } from "../policies/projectPolicy.js";
+import { canUpdateProject, canViewProject } from "../policies/projectPolicy.js";
 
 const handleResponse = (res, status, message, project = null) => {
   res.status(status).json({
@@ -13,12 +13,22 @@ const handleResponse = (res, status, message, project = null) => {
 export const viewProject = (req, res) => {
   const projectId = parseInt(req.params.id);
   const project = getProjectById(projectId, res);
-  console.log("Project is : ", project);
+  //   console.log("Project is : ", project);
   authorize(canViewProject, project)(req, res, () => {
     handleResponse(res, 200, "Project retrived successfully", project);
   });
 };
-export const updateProject = (res, req) => {};
+export const updateProject = (req, res) => {
+  const { name } = req.body;
+  const projectId = parseInt(req.params.id);
+  const project = getProjectById(projectId, res);
+
+  authorize(canUpdateProject, project)(req, res, () => {
+    const projectIndex = projects.findIndex((obj) => obj.id === project.id);
+    project[projectIndex].name = name;
+    handleResponse(res, 200, "Project updated successfully", project);
+  });
+};
 
 const getProjectById = (id, res) => {
   const project = projects.find((project) => project.id === id);
